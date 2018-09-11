b8bce7c34b457708f2c4c478b38cf12cc2d3b06a
wangwenwen <13554426960@163.com>
2018-09-11 10:15:14 +0800

    first commit

---
+++ src/main/java/com/yanjiashan/financialServicesManage/FinancialServicesManageApplication.java
@@ -0,0 +1,12 @@
+package com.yanjiashan.financialServicesManage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialServicesManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialServicesManageApplication.class, args);
+	}
+}
